f88efad518cd0224ef7cfd3f8666da1f07eb56c1
zachhealy <zachjhealy@gmail.com>
2023-08-22 16:25:12 -0500

    TOTAL REVAMP WOOHOO

---
+++ header.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    const header = document.querySelector(".sticky-header");
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+  
