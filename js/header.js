c70e637d946c79ecb6cfd3aa8eed758294e000ef
Zach Healy <zachjhealy@gmail.com>
2023-11-11 18:07:49 -0600

    Huge website update
    added gallery of designs, updated the header, updated the footer, and fixed some bugs

--- js/header.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-    const header = document.querySelector(".sticky-header");
-    if (window.scrollY > 0) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
-  });
-  
